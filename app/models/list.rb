ec1537c0b2cb8d398d688618715b48b54a43ef5e
Abdul Rafi <arafi15@gmail.com>
2022-08-22 20:58:51 +0800

    Push again

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :picture_url, presence: true
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_one_attached :photo
